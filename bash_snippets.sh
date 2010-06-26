48407cdbeb836769b6648cedd18ccb093680c8ad
Christoph Dittmann <github@christoph-d.de>
2010-06-26 18:13:27 +0200

    Add bash_snippets.sh.

---
+++ bash_snippets.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Just some code snippets I don't want to lose.
+
+set -e -u
+
+# Prints a stack trace to stderr.
+print_callstack() {
+    echo -e '*** stack trace [line function file] ***' >&2
+    local I=0
+    while caller $I >&2; do
+        let ++I
+    done
+    echo -e '*** end of trace ***' >&2
+}
+
+# Test the stack trace.
+g() { print_callstack; }
+f() { g; }
+f
+
+exit 0
