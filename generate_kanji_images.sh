8bf2714fbc294e325c9e6288cb6f9119053809ed
Christoph Dittmann <github@christoph-d.de>
2011-03-19 00:36:35 +0100

    Add kanji image generator.

---
+++ generate_kanji_images.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+size_in_pixels=300
+font=KanjiStrokeOrders-Medium
+x_shift=10
+y_shift=0
+
+i=1
+while read -n 1 kanji; do
+    [[ $kanji ]] || continue
+    filename="${i}_$kanji.png"
+    echo -n "Creating $filename..."
+    if convert -size ${size_in_pixels}x$size_in_pixels xc:white \
+        -font "$font" \
+        -gravity Center -pointsize "$(( $size_in_pixels - 10 ))" \
+        -draw "text ${x_shift},${y_shift} \"$kanji\"" "$filename"
+    then
+        echo 'done.'
+    else
+        echo 'failed!'
+    fi
+    let ++i
+done
